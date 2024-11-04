0e623df052ae263ce19fa983432ba2cd7e94aa53
Stephany Marciano Dias <130869859+stephanymdias@users.noreply.github.com>
2024-11-04 20:05:39 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import BuscaDeFilmes from './components/BusaDeFilmes';
+
+function App() {
+return (
+  <div className='App'>
+    <BuscaDeFilmes/>
+  </div>
+);
+}
+
+export default App
